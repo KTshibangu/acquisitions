b40aad13e28f9918109cb5eaf87b437f384794c6
KTshibangu <ktshibangu@gmail.com>
2026-05-29 13:09:33 +0200

    Implemented Security

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import router from '#routes/auth.routes.js';
 import { timestamp } from 'drizzle-orm/gel-core';
+import { securityMiddleware } from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 app.use(morgan('combined', {stream: {write: (message) => logger.info(message.trim())}}))
+
+app.use(securityMiddleware)
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!')
---
+++ src/config/arcjet.js
@@ -0,0 +1,22 @@
+import arcjet, { shield, detectBot, tokenBucket, slidingWindow } from "@arcjet/node";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: process.env.NODE_ENV === 'production' ? 'LIVE' : 'DRY_RUN',
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+        "CATEGORY:PREVIEW"
+      ],
+    }),
+    slidingWindow({
+        mode: 'LIVE',
+        interval: '25s',
+        max: 5
+    }),
+  ],
+});
+
+export default aj;
